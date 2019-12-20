6dcde2db3c3e01a3717c39c9d52067637fc0d6c2
francois.reynal <francois.raynal@ext.cdiscount.com>
2019-12-20 08:36:24 +0100

    initialisation

---
+++ src/test/java/org/gestion/banq/banq/BanqApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gestion.banq.banq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
